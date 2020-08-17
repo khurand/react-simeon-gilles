93b6aab2c02b627e6e1b12b72c5dda115786b7dd
khurand <kevin.hurand@gmail.com>
2020-08-17 15:54:40 +0200

    gsap with intersectionObserver

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { TweenMax, Power3 } from "gsap";
+import gsap from "gsap";
 import { Jump } from "react-jump";
 
 const Header = () => {
@@ -12,20 +12,26 @@ const Header = () => {
     LinkIcon3 = useRef(null);
 
   useEffect(() => {
-    TweenMax.from(headerItem, 1, {
+    gsap.from(headerItem, 1, {
       opacity: 0,
       y: "-100%",
-      ease: Power3.easeInOut,
+      ease: "Power4.easeInOut",
     });
-    TweenMax.to(headerItem, 0, {
+    gsap.to(headerItem, 0, {
       css: { visibility: "visible" },
     });
 
-    TweenMax.staggerFrom(
+    gsap.from(
       [LinkIcon1, LinkIcon2, LinkIcon3, LinkItem1, LinkItem2, LinkItem3],
-      1.3,
-      { opacity: 0, y: -500, ease: Power3.easeInOut },
-      0.2
+      1,
+      {
+        opacity: 0,
+        y: -500,
+        ease: "Power4.easeInOut",
+        stagger: {
+          amount: 0.5,
+        },
+      }
     );
   }, []);
 
--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Card, CardDeck } from "react-bootstrap";
+import gsap from "gsap";
 
 function Services() {
   const cards = [
@@ -22,8 +23,43 @@ function Services() {
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, nobis.",
     },
   ];
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          //do your actions here
+          gsap.to(ref.current, 2, {
+            opacity: 1,
+            x: 0,
+            ease: "Power4.easeOut",
+            stagger: {
+              amount: 0.5,
+            },
+          });
+          console.log("It works!");
+        }
+
+        if (entry.intersectionRatio > 0.6) {
+          observer.unobserve(entry.target);
+          console.log("stopped");
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.6,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+  }, [ref]);
+
   return (
-    <div className='card-container' id='services'>
+    <div className='card-container' id='services' ref={ref}>
       <CardDeck>
         <div className='row'>
           {cards.map((item, idx) => {
